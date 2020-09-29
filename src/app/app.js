083f1c78c558b294aaec8715770a8e74e84fad66
Do Duc Duy <doducduy.vietnam@gmail.com>
2020-09-30 01:03:11 +0700

    header mobile

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,26 @@
+const navNemu = document.getElementById("nav__menu");
+
+function intiToggleNavMenuEvent() {
+    const navToggle = document.getElementById("nav__toggle");
+    navToggle.addEventListener('click', function() {
+        navNemu.classList.toggle('show');
+    });
+}
+
+function initActiveLinkEvent() {
+    const navLinks = document.querySelectorAll('.nav__link');
+    let previousLink = document.querySelector('.active');
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            previousLink.classList.remove('active');
+            link.classList.add('active');
+            previousLink = link;
+            navNemu.classList.remove('show');
+        });
+    })
+}
+
 export const run = () => {
-    console.log('hello world');
+    intiToggleNavMenuEvent();
+    initActiveLinkEvent();
 };
